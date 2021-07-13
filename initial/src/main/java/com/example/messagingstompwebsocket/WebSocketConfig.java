9aa01aad277147248145d45946e9b3880b67c258
hyeon9mak <jinha3507@gmail.com>
2021-07-13 09:24:38 +0900

    refactor: 설정 클래스에 어노테이션 기입

--- initial/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
+++ initial/src/main/java/com/example/messagingstompwebsocket/WebSocketConfig.java
@@ -1,9 +1,13 @@
 package com.example.messagingstompwebsocket;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@EnableWebSocketMessageBroker
+@Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
